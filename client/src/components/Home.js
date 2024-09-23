0e07d1b95e7080ace94c674b53210b1c597cf298
SumatSingh <srmorena1802@gmail.com>
2024-09-23 16:17:04 +0530

    second

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,29 +1,174 @@
-import React from 'react'
+import React from 'react'; 
 import { Link } from 'react-router-dom';
-import { Fragment } from 'react'
+import { Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
+
 export default function Home() {
   return (
     <Fragment>
+      {/* Home Section */}
+      <section className="home py-5" id="home" style={{ backgroundColor: '#000', color: '#fff' }}>
+        <div className="container py-5">
 
-      <section className="home" id="home">
-        <div className="content">
-          <h3>its time to celebrate! the best <span> event organizers </span></h3>
-          <Link to="./SignIn" id="btn">Book Now</Link>
-        </div>
+          {/* Content Section */}
+          <div className="row align-items-center mb-5 py-5">
+            <div className="col-lg-6 text-center text-lg-start">
+              <h1 className="display-4 mb-4 fw-bold">
+                It's time to celebrate! The best <span className="text-warning">Event Organizers</span>
+              </h1>
+              <Link to="./SignIn" className="btn btn-warning btn-lg">
+                Book Now
+              </Link>
+            </div>
+            <div className="col-lg-6 mt-4 mt-lg-0 text-center py-5">
+              <img
+                src="assets/New folder/Venue/venue_13.jpg"
+                alt="Event Organizer"
+                className="img-fluid rounded shadow-sm py-5"
+                style={{ maxWidth: '100%', height: 'auto' }}
+              />
+            </div>
+          </div>
 
+          {/* Image Slider 1*/}
+          <div className="row g-4">
+            {/* First Image */}
+            <div className="col-md-6 col-lg-3">
+              <div className="card bg-dark text-white shadow-sm">
+                <img
+                  src="assets/images/homeImg/homeImg6.jpg"
+                  alt="Slide 1"
+                  className="card-img-top rounded"
+                  style={{ height: '250px', objectFit: 'cover' }}
+                />
+              </div>
+            </div>
 
-        <div className="swiper-container home-slider ">
-          <div className="swiper-wrapper ms-5">
-            <div className="swiper-slide me-5"><img src="assets/images/slidepic1.jpg" alt="" /></div>
-            <div className="swiper-slide  me-4"><img src="assets/images/slidepic3.jpg" alt=""/></div>
-            <div className="swiper-slide ms-2"><img src="assets/images/slidepic2.jpg" alt="" /></div>
-            {/* <div className="swiper-slide"><img src="assets/images/slidepic4.jpg" alt="" /></div>
-            <div className="swiper-slide"><img src="assets/images/slidepic1.jpg" alt="" /></div>
-            <div className="swiper-slide"><img src="assets/images/slidepic4.jpg" alt="" /></div> */}
+            {/* Second Image */}
+            <div className="col-md-6 col-lg-3">
+              <div className="card bg-dark text-white shadow-sm">
+                <img
+                  src="assets/images/homeImg/homeImg2.jpg"
+                  alt="Slide 2"
+                  className="card-img-top rounded"
+                  style={{ height: '250px', objectFit: 'cover' }}
+                />
+              </div>
+            </div>
+
+            {/* Third Image */}
+            <div className="col-md-6 col-lg-3">
+              <div className="card bg-dark text-white shadow-sm">
+                <img
+                  src="assets/images/homeImg/homeImg3.jpeg"
+                  alt="Slide 3"
+                  className="card-img-top rounded"
+                  style={{ height: '250px', objectFit: 'cover' }}
+                />
+              </div>
+            </div>
+
+            {/* Fourth Image */}
+            <div className="col-md-6 col-lg-3">
+              <div className="card bg-dark text-white shadow-sm">
+                <img
+                  src="assets/images/homeImg/homeImg4.jpg"
+                  alt="Slide 4"
+                  className="card-img-top rounded"
+                  style={{ height: '250px', objectFit: 'cover' }}
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* Image Slider 2*/}
+          <div className="row g-4">
+            {/* First Image */}
+            <div className="col-md-6 col-lg-3">
+              <div className="card bg-dark text-white shadow-sm">
+                <img
+                  src="assets/images/homeImg/homeImg4.jpg"
+                  alt="Slide 1"
+                  className="card-img-top rounded"
+                  style={{ height: '250px', objectFit: 'cover' }}
+                />
+              </div>
+            </div>
+
+            {/* Second Image */}
+            <div className="col-md-6 col-lg-3">
+              <div className="card bg-dark text-white shadow-sm">
+                <img
+                  src="assets/images/homeImg/homeImg3.jpeg"
+                  alt="Slide 2"
+                  className="card-img-top rounded"
+                  style={{ height: '250px', objectFit: 'cover' }}
+                />
+              </div>
+            </div>
+
+            {/* Third Image */}
+            <div className="col-md-6 col-lg-3">
+              <div className="card bg-dark text-white shadow-sm">
+                <img
+                  src="assets/images/homeImg/homeImg2.jpg"
+                  alt="Slide 3"
+                  className="card-img-top rounded"
+                  style={{ height: '250px', objectFit: 'cover' }}
+                />
+              </div>
+            </div>
+
+            {/* Fourth Image */}
+            <div className="col-md-6 col-lg-3">
+              <div className="card bg-dark text-white shadow-sm">
+                <img
+                  src="assets/images/homeImg/homeImg6.jpg"
+                  alt="Slide 4"
+                  className="card-img-top rounded"
+                  style={{ height: '250px', objectFit: 'cover' }}
+                />
+              </div>
+            </div>
           </div>
         </div>
-
       </section>
     </Fragment>
-  )
+  );
 }
+
+
+
+
+
+
+
+// import React from 'react'
+// import { Link } from 'react-router-dom';
+// import { Fragment } from 'react'
+// export default function Home() {
+//   return (
+//     <Fragment>
+
+//       <section className="home" id="home">
+//         <div className="content">
+//           <h3>its time to celebrate! the best <span> event organizers </span></h3>
+//           <Link to="./SignIn" id="btn">Book Now</Link>
+//         </div>
+
+
+//         <div className="swiper-container home-slider ">
+//           <div className="swiper-wrapper ms-5">
+//             <div className="swiper-slide me-5"><img src="assets/images/slidepic1.jpg" alt="" /></div>
+//             <div className="swiper-slide  me-4"><img src="assets/images/slidepic3.jpg" alt=""/></div>
+//             <div className="swiper-slide ms-2"><img src="assets/images/slidepic2.jpg" alt="" /></div>
+//             {/* <div className="swiper-slide"><img src="assets/images/slidepic4.jpg" alt="" /></div>
+//             <div className="swiper-slide"><img src="assets/images/slidepic1.jpg" alt="" /></div>
+//             <div className="swiper-slide"><img src="assets/images/slidepic4.jpg" alt="" /></div> */}
+//           </div>
+//         </div>
+
+//       </section>
+//     </Fragment>
+//   )
+// }
--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,80 +1,314 @@
-import React from 'react'
-import { Fragment } from 'react'
+import React from 'react';
+import { Fragment } from 'react';
+
 export default function Review() {
   return (
     <Fragment>
-      {/* start */}
-      <div>mt-5</div>
-            <div>mt-5</div>
-            <div>mt-5</div>   {/*for margin top full page */}
-            <div>mt-5</div>
-            <div>mt-5</div>
-            <div>mt-5</div>
+      {/* Add some spacing at the top */}
+      <div className="mt-5"></div>
 
-            {/* end  */}
-      
-       <section className="reivew" id="review"> 
-    
-    <h1 className="heading">client's <span>review</span></h1>
+      {/* Review Section */}
+      <section className="review py-5" id="review">
+        <h1 className="text-center display-4 mb-5">
+          Client's <span className="text-primary">Review</span>
+        </h1>
+        <h1 className="text-center display-4 mb-5">
+          Client's <span className="text-primary">Review</span>
+        </h1>
 
-    <div className="review-slider swiper-container">
+        {/* Review Slider Container */}
+        <div className="container">
+          <div className="row">
 
-        <div className="swiper-wrapper">
-
-
-            <div className="swiper-slide box me-5">
-                <i className="fas fa-quote-right"></i>
-                <div className="user">
-                    <img src="Assets/images/client_pic1.jpg" alt=""/>
-                    <div className="user-info">
-                        <h3>Falcon Sumat</h3>
-                        <span>happy clients</span>
+            {/* Individual Review Box */}
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card h-100 shadow-sm text-bg-dark">
+                <div className="card-body">
+                  <i className="fas fa-quote-right text-primary fa-2x mb-3"></i>
+                  <div className="d-flex align-items-center mb-3">
+                    <img
+                      src="Assets/images/client_pic1.jpg"
+                      alt="Client 1"
+                      className="rounded-circle me-3"
+                      style={{ width: '60px', height: '60px', objectFit: 'cover' }}
+                    />
+                    <div>
+                      <h5 className="mb-0">Falcon Sumat</h5>
+                      <small className="text-muted">Happy Client</small>
                     </div>
+                  </div>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum.
+                  </p>
                 </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum. Eos beatae eligendi, magni numquam nemo sed ut corrupti, ipsam iure nisi unde et assumenda perspiciatis voluptatibus nihil.</p>
+              </div>
             </div>
 
-            <div className="swiper-slide box">
-                <i className="fas fa-quote-right"></i>
-                <div className="user">
-                    <img src="Assets/images/client_pic2.jpeg" alt=""/>
-                    <div className="user-info">
-                        <h3>Arun Chourasiya</h3>
-                        <span>happy clients</span>
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card h-100 shadow-sm text-bg-dark">
+                <div className="card-body">
+                  <i className="fas fa-quote-right text-primary fa-2x mb-3"></i>
+                  <div className="d-flex align-items-center mb-3">
+                    <img
+                      src="Assets/images/client_pic2.jpeg"
+                      alt="Client 2"
+                      className="rounded-circle me-3"
+                      style={{ width: '60px', height: '60px', objectFit: 'cover' }}
+                    />
+                    <div>
+                      <h5 className="mb-0">Arun Chourasiya</h5>
+                      <small className="text-muted">Happy Client</small>
                     </div>
+                  </div>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum.
+                  </p>
                 </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum. Eos beatae eligendi, magni numquam nemo sed ut corrupti, ipsam iure nisi unde et assumenda perspiciatis voluptatibus nihil.</p>
+              </div>
             </div>
 
-            <div className="swiper-slide box">
-                <i className="fas fa-quote-right"></i>
-                <div className="user">
-                    <img src="Assets/images/client_pic3.jpeg" alt=""/>
-                    <div className="user-info">
-                        <h3>Rahul Singh</h3>
-                        <span>happy clients</span>
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card h-100 shadow-sm text-bg-dark">
+                <div className="card-body">
+                  <i className="fas fa-quote-right text-primary fa-2x mb-3"></i>
+                  <div className="d-flex align-items-center mb-3">
+                    <img
+                      src="Assets/images/client_pic3.jpeg"
+                      alt="Client 3"
+                      className="rounded-circle me-3"
+                      style={{ width: '60px', height: '60px', objectFit: 'cover' }}
+                    />
+                    <div>
+                      <h5 className="mb-0">Rahul Singh</h5>
+                      <small className="text-muted">Happy Client</small>
                     </div>
+                  </div>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum.
+                  </p>
                 </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum. Eos beatae eligendi, magni numquam nemo sed ut corrupti, ipsam iure nisi unde et assumenda perspiciatis voluptatibus nihil.</p>
+              </div>
             </div>
 
-            <div className="swiper-slide box">
-                <i className="fas fa-quote-right"></i>
-                <div className="user">
-                    <img src="Assets/images/client_pic4.jpeg" alt=""/>
-                    <div className="user-info">
-                        <h3>Ravi Patel</h3>
-                        <span>happy clients</span>
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card h-100 shadow-sm text-bg-dark">
+                <div className="card-body">
+                  <i className="fas fa-quote-right text-primary fa-2x mb-3"></i>
+                  <div className="d-flex align-items-center mb-3">
+                    <img
+                      src="Assets/images/client_pic4.jpeg"
+                      alt="Client 4"
+                      className="rounded-circle me-3"
+                      style={{ width: '60px', height: '60px', objectFit: 'cover' }}
+                    />
+                    <div>
+                      <h5 className="mb-0">Ravi Patel</h5>
+                      <small className="text-muted">Happy Client</small>
                     </div>
+                  </div>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum.
+                  </p>
                 </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum. Eos beatae eligendi, magni numquam nemo sed ut corrupti, ipsam iure nisi unde et assumenda perspiciatis voluptatibus nihil.</p>
+              </div>
             </div>
 
+
+
+            
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card h-100 shadow-sm text-bg-dark">
+                <div className="card-body">
+                  <i className="fas fa-quote-right text-primary fa-2x mb-3"></i>
+                  <div className="d-flex align-items-center mb-3">
+                    <img
+                      src="Assets/images/client_pic4.jpeg"
+                      alt="Client 4"
+                      className="rounded-circle me-3"
+                      style={{ width: '60px', height: '60px', objectFit: 'cover' }}
+                    />
+                    <div>
+                      <h5 className="mb-0">Ravi Patel</h5>
+                      <small className="text-muted">Happy Client</small>
+                    </div>
+                  </div>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card h-100 shadow-sm text-bg-dark">
+                <div className="card-body">
+                  <i className="fas fa-quote-right text-primary fa-2x mb-3"></i>
+                  <div className="d-flex align-items-center mb-3">
+                    <img
+                      src="Assets/images/client_pic3.jpeg"
+                      alt="Client 3"
+                      className="rounded-circle me-3"
+                      style={{ width: '60px', height: '60px', objectFit: 'cover' }}
+                    />
+                    <div>
+                      <h5 className="mb-0">Rahul Singh</h5>
+                      <small className="text-muted">Happy Client</small>
+                    </div>
+                  </div>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card h-100 shadow-sm text-bg-dark">
+                <div className="card-body">
+                  <i className="fas fa-quote-right text-primary fa-2x mb-3"></i>
+                  <div className="d-flex align-items-center mb-3">
+                    <img
+                      src="Assets/images/client_pic2.jpeg"
+                      alt="Client 2"
+                      className="rounded-circle me-3"
+                      style={{ width: '60px', height: '60px', objectFit: 'cover' }}
+                    />
+                    <div>
+                      <h5 className="mb-0">Arun Chourasiya</h5>
+                      <small className="text-muted">Happy Client</small>
+                    </div>
+                  </div>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card h-100 shadow-sm text-bg-dark">
+                <div className="card-body">
+                  <i className="fas fa-quote-right text-primary fa-2x mb-3"></i>
+                  <div className="d-flex align-items-center mb-3">
+                    <img
+                      src="Assets/images/client_pic1.jpg"
+                      alt="Client 1"
+                      className="rounded-circle me-3"
+                      style={{ width: '60px', height: '60px', objectFit: 'cover' }}
+                    />
+                    <div>
+                      <h5 className="mb-0">Falcon Sumat</h5>
+                      <small className="text-muted">Happy Client</small>
+                    </div>
+                  </div>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum.
+                  </p>
+                </div>
+              </div>
+            </div>
+  
+
+          </div>
         </div>
-
-    </div>
-
-</section>
+      </section>
     </Fragment>
-  )
+  );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import { Fragment } from 'react'
+// export default function Review() {
+//   return (
+//     <Fragment>
+//       {/* start */}
+//       <div>mt-5</div>
+//             <div>mt-5</div>
+//             <div>mt-5</div>   {/*for margin top full page */}
+//             <div>mt-5</div>
+//             <div>mt-5</div>
+//             <div>mt-5</div>
+
+//             {/* end  */}
+      
+//        <section className="reivew" id="review"> 
+    
+//     <h1 className="heading">client's <span>review</span></h1>
+
+//     <div className="review-slider swiper-container">
+
+//         <div className="swiper-wrapper">
+
+
+//             <div className="swiper-slide box me-5">
+//                 <i className="fas fa-quote-right"></i>
+//                 <div className="user">
+//                     <img src="Assets/images/client_pic1.jpg" alt=""/>
+//                     <div className="user-info">
+//                         <h3>Falcon Sumat</h3>
+//                         <span>happy clients</span>
+//                     </div>
+//                 </div>
+//                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum. Eos beatae eligendi, magni numquam nemo sed ut corrupti, ipsam iure nisi unde et assumenda perspiciatis voluptatibus nihil.</p>
+//             </div>
+
+//             <div className="swiper-slide box">
+//                 <i className="fas fa-quote-right"></i>
+//                 <div className="user">
+//                     <img src="Assets/images/client_pic2.jpeg" alt=""/>
+//                     <div className="user-info">
+//                         <h3>Arun Chourasiya</h3>
+//                         <span>happy clients</span>
+//                     </div>
+//                 </div>
+//                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum. Eos beatae eligendi, magni numquam nemo sed ut corrupti, ipsam iure nisi unde et assumenda perspiciatis voluptatibus nihil.</p>
+//             </div>
+
+//             <div className="swiper-slide box">
+//                 <i className="fas fa-quote-right"></i>
+//                 <div className="user">
+//                     <img src="Assets/images/client_pic3.jpeg" alt=""/>
+//                     <div className="user-info">
+//                         <h3>Rahul Singh</h3>
+//                         <span>happy clients</span>
+//                     </div>
+//                 </div>
+//                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum. Eos beatae eligendi, magni numquam nemo sed ut corrupti, ipsam iure nisi unde et assumenda perspiciatis voluptatibus nihil.</p>
+//             </div>
+
+//             <div className="swiper-slide box">
+//                 <i className="fas fa-quote-right"></i>
+//                 <div className="user">
+//                     <img src="Assets/images/client_pic4.jpeg" alt=""/>
+//                     <div className="user-info">
+//                         <h3>Ravi Patel</h3>
+//                         <span>happy clients</span>
+//                     </div>
+//                 </div>
+//                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor dicta cum. Eos beatae eligendi, magni numquam nemo sed ut corrupti, ipsam iure nisi unde et assumenda perspiciatis voluptatibus nihil.</p>
+//             </div>
+
+//         </div>
+
+//     </div>
+
+// </section>
+//     </Fragment>
+//   )
+// }
